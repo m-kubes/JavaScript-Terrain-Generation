cea4f42541bf18eaa489f18eba07840a653b0541
m-kubes <michaelkubiak5@gmail.com>
2026-03-16 18:05:37 -0400

    Finalized generation math

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,98 @@ import tiles from './tiles.json' with { type: "json" };
 const canvas = document.getElementById('main-canvas');
 const ctx = canvas.getContext("2d");
 
-const drawer = new TileDrawer(ctx, 100, 5)
+const tile_size = 100
+const line_width = 7
+const width = 100
+const height = 100
+const biome_zoom = 6
+const canvas_padding = 5
+const height_variance = 9
+const tree_chance = 0.45
+
+// instantiate stuff
+const drawer = new TileDrawer(ctx, tile_size, line_width)
+noise.seed(Math.random());
+
+// center all tiles on screen
+canvas.width = width * tile_size + (canvas_padding * 2)
+canvas.height = height * tile_size + (canvas_padding * 2)
+ctx.translate((width * tile_size * 0.5) + canvas_padding, (height * tile_size * 0.25) + canvas_padding)
+
+// places this tile if GREATER THAN the float
+// water always places below the lowest threshold
+const tile_thresholds = {
+	'peak': 0.7,
+	'mountain': 0.645,
+	'plains': 0.44,
+	'beach': 0.4
+}
 
 
-for (let x = 0; x < 100; x++) {
-    for (let z = 0; z < 100; z++) {
-        drawer.draw_tile(x, 0, z, tiles['grass'])
+// make visible edges fatter
+function make_edge(x, y, z, tile) {
+    if (x == width - 1 || z == height - 1) {
+        const edge_tile = tiles[tile['edge_tile']] ?? tile
+        for (let i = 1; i < y; i++) {
+            scene_tiles.push([x,y-i,z,edge_tile])
+        }
     }
+}
+
+
+// create list of tiles
+const scene_tiles = []
+for (let x = 0; x < height; x++) {
+	for (let z = 0; z < width; z++) {
+		// get noise values and make them between 0 and 1
+		const biome_noise_value = (noise.perlin2(x/(width / biome_zoom), z/(height / biome_zoom)) + 1) / 2
+		const forest_noise_value = (noise.perlin2(x/(width / biome_zoom), z/(height / biome_zoom) + 50) + 1) / 2
+
+		// get y value of tile from noise
+		// this also centers the terrain on the screen
+		let y = Math.max(Math.floor(biome_noise_value * height_variance), 3)
+
+		// get type of tile from noise values
+		// also tweaks y values to make mountains a little taller
+		let tile
+		if (biome_noise_value > tile_thresholds['peak']) {
+			tile = tiles['dark_stone']
+			y += 2
+			scene_tiles.push([x,y-1,z,tile])
+			scene_tiles.push([x,y-2,z,tile])
+
+        } else if (biome_noise_value > tile_thresholds['mountain']) {
+			tile = tiles['stone']
+			y += 1
+			scene_tiles.push([x,y-1,z,tile])
+
+        } else if (biome_noise_value > tile_thresholds['plains']) {
+			tile = tiles['grass']
+
+			// place trees in forests
+			if (forest_noise_value < 0.5 && Math.random() < tree_chance) {
+				scene_tiles.push([x,y+1,z,tiles['tree']])
+            }
+
+			// account for water gap when going from grass straight to water
+			if (biome_noise_value > 0.46) {
+				scene_tiles.push([x,y-1,z,tiles['dirt']])
+            }
+
+        } else if (biome_noise_value > tile_thresholds['beach']) {
+			tile = tiles['sand']
+        } else {
+			tile = tiles['water']
+        }
+
+		make_edge(x,y,z,tile)
+		scene_tiles.push([x,y,z,tile])
+    }
+}
+
+// makes sure sorting orders are correct
+const sorted_tiles = scene_tiles.sort((a, b) => TileDrawer.sort_order(b) < TileDrawer.sort_order(a) ? 1 : -1)
+
+for (let tile of sorted_tiles) {
+	drawer.draw_tile(tile[0],tile[1],tile[2],tile[3])
 }
--- scripts/tile_drawer.js
+++ scripts/tile_drawer.js
@@ -1,33 +1,34 @@
-// get sort order
-// things with higher sort orders are rendered last (on top of everything else)
-function sort_order(tile, ignoreType=False) {
-	if (!ignoreType) {
-		switch (tile[3]['type']) {
-			case 'tree':
-				return (sort_order(tile, True) + sort_order((tile[0],tile[1]+1,tile[2],tile[3]), True)) / 2
-        }
-    }
-
-	return ((tile[0] + tile[2]) * -1) + tile[1]
-}
-
 export default class TileDrawer {
-	constructor (ctx, tile_size, outline_width=2) {
+	constructor (ctx, tile_size, outline_width) {
 		this.tile_size = tile_size
 		this.ctx = ctx
-		this.ctx.lineWidth = outline_width
+		this.outline_width = outline_width
     }
+
+	// get sort order
+	// things with higher sort orders are rendered last (on top of everything else)
+	static sort_order(tile, ignoreType=false) {
+		if (!ignoreType) {
+			switch (tile['type']) {
+				case 'tree':
+					return (this.sort_order(tile, True) + this.sort_order((tile[0],tile[1]+1,tile[2],tile[3]), True)) / 2
+			}
+		}
+
+		return ((tile[0] + tile[2])) + tile[1]
+	}
 
 	// converts 3d coordinates to an isometric grid
 	to_isometric(x, y, z, tile_size) {
 		const iso_x = x * (0.5 * tile_size) + z * (-0.5 * tile_size)
-		const iso_y = x * (0.25 * tile_size) + z * (0.25 * tile_size) + y * (0.5 * tile_size)
+		const iso_y = x * (0.25 * tile_size) + z * (0.25 * tile_size) - y * (0.5 * tile_size)
 		return [iso_x, iso_y]
     }
 
 	// rectange
 	draw_rect(x, y, width, height) {
 		this.ctx.beginPath()
+		this.ctx.lineWidth = this.outline_width
 		this.ctx.rect(x - 0.5 * width, y - 0.5 * height, width, height)
 		this.ctx.fill()
 		this.ctx.stroke()
@@ -36,6 +37,7 @@ export default class TileDrawer {
 	// draws the top of a tile
 	draw_top_tile_face(x, y, size) {
 		this.ctx.beginPath()
+		this.ctx.lineWidth = this.outline_width
 		this.ctx.moveTo(x, y)
 		this.ctx.lineTo(x + (0.5 * size), y - (0.25 * size))
 		this.ctx.lineTo(x, y - (0.5 * size))
@@ -48,6 +50,7 @@ export default class TileDrawer {
 	// draws the right or left side of a tile
 	draw_side_tile_face(x, y, width, height, side=1) {
 		this.ctx.beginPath()
+		this.ctx.lineWidth = this.outline_width
 		this.ctx.moveTo(x, y)
 		this.ctx.lineTo(x - 0.5 * width * side, y - 0.25 * width)
 		this.ctx.lineTo(x - 0.5 * width * side, y - 0.25 * width + 0.5 * height)
@@ -58,19 +61,20 @@ export default class TileDrawer {
     }
 
 	// tree leaves bro
-	draw_tree_leaves(size) {
-		// const t = this.t
-		// x, y = t.pos()
-		// t.begin_fill()
-		// this.ctx.moveTo(x - 0.4 * size, y)
-		// t.goto(x + 0.4 * size, y)
-		// t.goto(x + 0.2 * size, y + 0.5 * size)
-		// t.goto(x + 0.325 * size, y + 0.5 * size)
-		// t.goto(x, y + 1.25 * size)
-		// t.goto(x - 0.325 * size, y + 0.5 * size)
-		// t.goto(x - 0.2 * size, y + 0.5 * size)
-		// t.goto(x - 0.4 * size, y)
-		// t.end_fill()
+	draw_tree_leaves(x, y, size) {
+		this.ctx.beginPath()
+		this.ctx.lineWidth = this.outline_width
+		this.ctx.moveTo(x - 0.4 * size, y)
+		this.ctx.lineTo(x + 0.4 * size, y)
+		this.ctx.lineTo(x + 0.2 * size, y - 0.5 * size)
+		this.ctx.lineTo(x + 0.325 * size, y - 0.5 * size)
+		this.ctx.lineTo(x, y - 1.25 * size)
+		this.ctx.lineTo(x - 0.325 * size, y - 0.5 * size)
+		this.ctx.lineTo(x - 0.2 * size, y - 0.5 * size)
+		this.ctx.lineTo(x - 0.4 * size, y)
+		this.ctx.closePath()
+		this.ctx.stroke()
+		this.ctx.fill()
 	}
 
 
@@ -97,19 +101,18 @@ export default class TileDrawer {
 
 				break
 			case 'liquid':
-				this.ctx.moveTo(x, y - 0.15 * this.tile_size)
 				// draw top face
 				this.ctx.strokeStyle = tile['outline_color']
 				this.ctx.fillStyle = tile['top_face_color']
-				this.draw_top_tile_face(x, y, this.tile_size)
+				this.draw_top_tile_face(x, y + 0.15 * this.tile_size, this.tile_size)
 
 				// draw left side face
 				this.ctx.fillStyle = tile['left_face_color']
-				this.draw_side_tile_face(x, y, this.tile_size, this.tile_size - 0.3 * this.tile_size, 1)
+				this.draw_side_tile_face(x, y + 0.15 * this.tile_size, this.tile_size, this.tile_size - 0.3 * this.tile_size, 1)
 
 				// draw right side face
 				this.ctx.fillStyle = tile['outline_color']
-				this.draw_side_tile_face(x, y, this.tile_size, this.tile_size - 0.3 * this.tile_size, -1)
+				this.draw_side_tile_face(x, y + 0.15 * this.tile_size, this.tile_size, this.tile_size - 0.3 * this.tile_size, -1)
 
 				break
 			case 'grasstop':
@@ -126,37 +129,31 @@ export default class TileDrawer {
 				this.ctx.fillStyle = tile['top_outline_color']
 				this.draw_side_tile_face(x, y, this.tile_size, this.tile_size * 0.3, -1)
 
-				// move cursor down to draw bottom part
-				this.ctx.moveTo(x,y - 0.15 * this.tile_size)
-
 				// draw bottom left face
 				this.ctx.strokeStyle = tile['bottom_outline_color']
 				this.ctx.fillStyle = tile['bottom_side_color']
-				this.draw_side_tile_face(x, y, this.tile_size, this.tile_size * 0.7, 1)
+				this.draw_side_tile_face(x, y + 0.15 * this.tile_size, this.tile_size, this.tile_size * 0.7, 1)
 
 				// draw bottom right face
 				this.ctx.fillStyle = tile['bottom_outline_color']
-				this.draw_side_tile_face(x, y, this.tile_size, this.tile_size * 0.7, -1)
+				this.draw_side_tile_face(x, y + 0.15 * this.tile_size, this.tile_size, this.tile_size * 0.7, -1)
 
 				break
 			case 'tree':
 				// trunk
 				this.ctx.strokeStyle = tile['trunk_outline_color']
 				this.ctx.fillStyle = tile['trunk_color']
-				this.ctx.moveTo(x, y - 0.125 * this.tile_size)
-				this.draw_rect(0.25 * this.tile_size, 0.25 * this.tile_size)
+				this.draw_rect(x, y + 0.125 * this.tile_size, 0.25 * this.tile_size, 0.25 * this.tile_size)
 
 				// part of trunk in shadow from the leaves
 				this.ctx.strokeStyle = tile['trunk_shaded_outline_color']
 				this.ctx.fillStyle = tile['trunk_shaded_color']
-				this.ctx.moveTo(x, y + 0.125 * this.tile_size)
-				this.draw_rect(0.25 * this.tile_size, 0.25 * this.tile_size)
+				this.draw_rect(x, y - 0.125 * this.tile_size, 0.25 * this.tile_size, 0.25 * this.tile_size)
 
 				// leaves
 				this.ctx.strokeStyle = tile['leaves_outline_color']
 				this.ctx.fillStyle = tile['leaves_color']
-				this.ctx.moveTo(x,y + 0.125 * this.tile_size)
-				this.draw_tree_leaves(this.tile_size)
+				this.draw_tree_leaves(x, y - 0.125 * this.tile_size, this.tile_size)
 
 				break
         }
